e277db94b0161c70e3427a446f06807e6499a68d
Heitor Rosa <heitorrs.dev@gmail.com>
2025-08-28 18:10:03 -0300

    feat(TEPOO): add Tratamento de Exceções Personalizadas exercise

---
+++ TEPOO/Tratamento de Exceções Personalizadas/TesteContaBancaria.java
@@ -0,0 +1,19 @@
+public class TesteContaBancaria {
+  public static void main(String[] args) {
+    ContaBancaria conta = new ContaBancaria(1000.0);
+
+    try {
+      System.out.println("\nSaldo inicial: " + conta.getSaldo());
+      conta.sacar(600.0);
+
+      System.out.println("Saldo após saque de 600: " + conta.getSaldo());
+      conta.sacar(500.0); // Isso deve lançar a exceção
+
+      System.out.println("Saldo após saque de 500: " + conta.getSaldo());
+    } catch (SaldoInsuficienteException e) {
+      System.out.println("Erro: " + e.getMessage());
+    }
+
+    System.out.println("Saldo final: " + conta.getSaldo());
+  }
+}
