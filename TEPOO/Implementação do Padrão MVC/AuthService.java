07184798ce945e29d130aca4e1515bfd0a619d0a
Heitor Rosa <heitorrs.dev@gmail.com>
2025-09-06 17:45:25 -0300

    feat(TEPOO): add  Implementação do Padrão MVC exercise

---
+++ TEPOO/Implementação do Padrão MVC/AuthService.java
@@ -0,0 +1,6 @@
+public class AuthService {
+  // Lógica de autenticação
+  public boolean authenticate(User user) {
+    return "admin".equals(user.getUsername()) && "123".equals(user.getPassword());
+  }
+}
---
+++ TEPOO/Implementação do Padrão MVC/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      LoginView view = new LoginView();
+      AuthService service = new AuthService();
+      new LoginController(service, view);
+      view.setVisible(true);
+    });
+  }
+}
